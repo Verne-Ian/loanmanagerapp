f01b5a176c2f5da748db604225c32b75fff8734b
verne-andy <ianandy778@gmail.com>
2022-10-18 10:04:18 +0300

    Added second interface with no design.

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,83 @@ class MyHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: AppBar(
-      backgroundColor: Colors.blue[300],
-      title: Text(
-        "Loan Manager",
-        textAlign: TextAlign.center
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Loan Management Center",
+        ),
+        centerTitle: true,
+
       ),
-      )
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Icon(
+                Icons.account_circle,
+                size: 120.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                child: Text(
+                  "Username",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                  )
+
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(child: Text(
+                  "Password",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold
+                  )
+              )),
+            ),
+            Expanded(
+              child: FloatingActionButton(onPressed: () {
+                runApp(MaterialApp(
+                  home: signup()
+                ));
+
+              },
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.0
+
+                ),
+              ),
+              ),
+            )
+
+          ],
+        ),
+      ),
 
     );
   }
 }
+class signup extends StatelessWidget {
+  const signup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
+
 
