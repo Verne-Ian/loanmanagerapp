5a33a7a6707e0a2eee124f46d95f477d2e20462c
verne-andy <ianandy778@gmail.com>
2022-10-29 22:28:04 +0300

    Major updates to the UI and corrections in the code.

--- lib/main.dart
+++ lib/main.dart
@@ -15,57 +15,76 @@ class MyHome extends StatelessWidget {
         title: const Text(
           "Loan Management Center",
         ),
+        backgroundColor: Colors.black87,
         centerTitle: true,
 
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.account_circle,
-              size: 120.0,
-            ),
-            const Divider(height: 70.0,),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                "Username",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.account_circle,
+                  size: 120.0,
+                ),
+                const Divider(height: 70.0,),
+                const SizedBox(height: 15.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      prefixIcon: Icon(
+                        Icons.account_circle,
+                        size: 15.0,
+                      ),
+                      labelText: "Username",
+                      hintText: "Enter username.",
+                    hintStyle: TextStyle(
+                      fontSize: 10
+                    )
+                  ),
+                ),
+                const SizedBox(height: 15.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      prefixIcon: Icon(
+                        Icons.password,
+                        size: 15.0,
+                      ),
+                      labelText: "Password",
+                      hintText: "Enter password",
+                      hintStyle: TextStyle(
+                          fontSize: 10
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+                  child: ElevatedButton( onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const signup()),
+                    );},
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0
+                    ),
+                  ),
+                  ),
                 )
 
-              ),
+              ],
             ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                  "Password",
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold
-                  )
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
-              child: ElevatedButton( onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const signup()),
-                );},
-              child: const Text(
-                "Sign Up",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12.0
-                ),
-              ),
-              ),
-            )
-
-          ],
+          ),
         ),
       ),
 
@@ -77,11 +96,11 @@ class signup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        title: const Text(
+        backgroundColor: Colors.black,
+        title: Text(
           "New User Registration",
           style: TextStyle(
             fontWeight: FontWeight.bold,
@@ -93,82 +112,121 @@ class signup extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:  [
-          const Text(
-            "First Name"
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: "Enter first name.",
-            ),
-          ),
-          const SizedBox(height: 15.0,),
-          const Text(
-              "Last Name"
-          ),
-          const TextField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: "Enter last name."
-            ),
-          ),
-          const SizedBox(height: 15.0,),
-          const Text(
-              "Phone number:"
-          ),
-          const TextField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: "Enter phone number."
-            ),
-          ),
-          const SizedBox(height: 15.0,),
-          const Text(
-              "Email: "
-          ),
-          const TextField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: "Enter your email."
-            ),
-          ),
-          const SizedBox(height: 15.0,),
-          const Text(
-              "Address: "
-          ),
-          const TextField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: "Enter your current."
-            ),
-          ),
-          const SizedBox(height: 50.0,),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ElevatedButton(onPressed: (){
-                    Navigator.pop(context);
-                  },style: const ButtonStyle(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+              SizedBox(height: 15.0,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                    prefixIcon: Icon(
+                      Icons.account_circle,
+                      size: 15.0,
                     ),
-                      child: const Icon(
-                        Icons.home,
-
-                      ), ),
+                  labelText: "First name",
+                  hintText: "Enter first name.",
+                    hintStyle: TextStyle(
+                        fontSize: 10
+                    )
                 ),
-                Expanded(
-                child: ElevatedButton(onPressed: (){
+              ),
+              SizedBox(height: 15.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    prefixIcon: Icon(
+                      Icons.account_circle,
+                      size: 15.0,
+                    ),
+                    labelText: "Last name",
+                  hintText: "Enter Last name.",
+                    hintStyle: TextStyle(
+                        fontSize: 10
+                    )
+                ),
+              ),
+              SizedBox(height: 15.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    prefixIcon: Icon(
+                      Icons.phone,
+                      size: 15.0,
+                    ),
+                    labelText: "Phone number",
+                  hintText: "+256xxxxxxxxx",
+                    hintStyle: TextStyle(
+                        fontSize: 10
+                    )
+                ),
+              ),
+              SizedBox(height: 15.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    prefixIcon: Icon(
+                      Icons.alternate_email,
+                        size: 15.0
+                    ),
+                    labelText: "Email",
+                  hintText: "you@example.com",
+                    hintStyle: TextStyle(
+                        fontSize: 10
+                    ),
+                  contentPadding: EdgeInsets.all(10)
+                ),
+              ),
+              SizedBox(height: 15.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    prefixIcon: Icon(
+                      Icons.local_post_office,
+                        size: 15.0
+                    ),
+                    labelText: "Address",
+                  hintText: "Physical address",
+                    hintStyle: TextStyle(
+                        fontSize: 10
+                    )
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ButtonBar(
+                      buttonHeight: 20,
+                      children: [
+                        ElevatedButton(onPressed: (){
 
-                },
-                    child: const Icon(
-                        Icons.save_alt
-                    ) ))
-              ],
-            ),
-        ],
+                        },
+                            child: Text(
+                              "Submit",
+                                style: TextStyle(
+                                  color: Colors.black
+                                ),
+                            ),)
+                      ],
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ),
       ),
 
     );
